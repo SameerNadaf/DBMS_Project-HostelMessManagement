4113a414e728d4fb2758a0e9bfb8a67ac8b24f4a
Sameer Nadaf <nsameernadaf@gmail.com>
2024-03-01 00:17:35 +0530

    Add to git

---
+++ html/footer.php
@@ -0,0 +1,4 @@
+    <div id="footer" style="width:100%;margin-left:-1px; padding-bottom:20px;"><br>
+        <center><p style="color: white; font-family: cursive; margin-top: 10px; font-size: 15px">HOSTEL MESS<br>Shivbasav Nagar, Belgaum , 590010</p></center>
+        <center><p style="width:100%;margin-left:-1px; color: white;">Copyright &copy; 2024 -All Rights Reserved</p></center>
+    </div>  
